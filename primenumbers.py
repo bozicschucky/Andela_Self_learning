f4bb80d1e5a1e74d0492e7cf31c3252e363f50db
Ssekitto charlse <bozicslxye1@gmail.com>
2017-07-03 20:10:02 +0300

    Add files via upload
    The file added some brief comment description of the big O notation of the function used.

--- primenumbers.py
+++ primenumbers.py
@@ -1,6 +1,6 @@
 import time
 
-def prime_numbers(number):
+def primenumbers(number):
     start = time.time()
     primes = set() # initialized empty set to store the values to be added to the set
     if type(number) == int and number > 0:
@@ -17,9 +17,15 @@ def prime_numbers(number):
     return ("The prime numbers are " +str(primes) + ". The program ran for " + str(time.time()-start))
 
 if __name__ == '__main__':
-    print prime_numbers(5) # running time 4.48226928711e-05
-    print prime_numbers(10) # running time 1.50203704834e-05
-    print prime_numbers(100) # running time 0.000203132629395
-    print prime_numbers(500) # running time 0.00300407409668
+    #print prime_numbers(2)
+    print primenumbers(5) # running time 4.48226928711e-05
+    print primenumbers(10) # running time 1.50203704834e-05
+    print primenumbers(100) # running time 0.000203132629395
+    print primenumbers(500) # running time 0.00300407409668
 
 	# the big O of the above function takes more time to print the prime numbers with an increase in the number of inputs in the function
+
+
+""" The big O of the number basically follows n^2 since the loops are repeating and execute a given number of times.
+
+so based on the inputs the big-O of the above function is n^2 keeping the constant steps fixed."""
